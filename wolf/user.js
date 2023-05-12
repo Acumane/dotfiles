e6b4c6b17fdf51e00b0b2de9aa8442dfe87d3512
Bren <94324138+Acumane@users.noreply.github.com>
2023-05-11 18:18:52 -0700

    Browser ext (Day 1)

---
+++ wolf/user.js
@@ -0,0 +1,13 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("ui.key.menuAccessKeyFocuses", false);
+
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("browser.theme.dark-private-windows", false);
+user_pref("devtools.debugger.prompt-connection", false);
+user_pref("browser.uidensity", 0); // Normal UI density
+user_pref("full-screen-api.warning.timeout", 0);
+
+user_pref("gnomeTheme.hideSingleTab", true);
+user_pref("gnomeTheme.systemIcons", true);
+user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
+user_pref("gnomeTheme.hideUnifiedExtensions", true);
