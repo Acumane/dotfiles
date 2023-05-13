2468b807c146c2ff7dc1fcbc3ac4f2de63076545
Bren <94324138+Acumane@users.noreply.github.com>
2023-05-12 23:31:22 -0700

    Day 2ish 🤷
    misc

--- wolf/user.js
+++ wolf/user.js
@@ -4,10 +4,11 @@ user_pref("ui.key.menuAccessKeyFocuses", false);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.theme.dark-private-windows", false);
 user_pref("devtools.debugger.prompt-connection", false);
+user_pref("devtools.debugger.remote-enabled", true);
 user_pref("browser.uidensity", 0); // Normal UI density
 user_pref("full-screen-api.warning.timeout", 0);
 
 user_pref("gnomeTheme.hideSingleTab", true);
 user_pref("gnomeTheme.systemIcons", true);
 user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
-user_pref("gnomeTheme.hideUnifiedExtensions", true);
+// user_pref("gnomeTheme.hideUnifiedExtensions", true);
