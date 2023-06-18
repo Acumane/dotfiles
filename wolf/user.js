be824ea753fe3607fd0d73292a09944d260b661a
Bren <94324138+Acumane@users.noreply.github.com>
2023-06-18 16:12:46 -0400

    Discord css, terminal colors, misc.

--- wolf/user.js
+++ wolf/user.js
@@ -5,9 +5,10 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.theme.dark-private-windows", false);
 user_pref("devtools.debugger.prompt-connection", false);
 user_pref("devtools.debugger.remote-enabled", true);
-user_pref("browser.uidensity", 0); // Normal UI density
 user_pref("full-screen-api.warning.timeout", 0);
+user_pref("mousewheel.default.delta_multiplier_y", 30);
 
+user_pref("browser.uidensity", 0); // Normal UI density
 user_pref("gnomeTheme.hideSingleTab", true);
 user_pref("gnomeTheme.systemIcons", true);
 user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
