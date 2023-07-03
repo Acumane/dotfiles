5450889aaf82728817fa11ff17eb63b70166e24d
Bren <94324138+Acumane@users.noreply.github.com>
2023-07-02 23:01:36 -0400

    PDF viewer, PWA tweaks

--- wolf/user.js
+++ wolf/user.js
@@ -7,9 +7,13 @@ user_pref("devtools.debugger.prompt-connection", false);
 user_pref("devtools.debugger.remote-enabled", true);
 user_pref("full-screen-api.warning.timeout", 0);
 user_pref("mousewheel.default.delta_multiplier_y", 30);
+user_pref("gfx.webrender.all", true);
 
 user_pref("browser.uidensity", 0); // Normal UI density
 user_pref("gnomeTheme.hideSingleTab", true);
 user_pref("gnomeTheme.systemIcons", true);
 user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
 // user_pref("gnomeTheme.hideUnifiedExtensions", true);
+
+user_pref("firefoxpwa.enableHidingIconBar", true);
+user_pref("firefoxpwa.displayUrlBar", 1);
