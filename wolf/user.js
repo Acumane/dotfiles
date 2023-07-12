b8e1e46aa77f873612864d2500089e5a9b637333
Bren <94324138+Acumane@users.noreply.github.com>
2023-07-12 01:20:52 -0400

    LF hw decode, zsh nav, Edge PDF

--- wolf/user.js
+++ wolf/user.js
@@ -17,3 +17,7 @@ user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
 
 user_pref("firefoxpwa.enableHidingIconBar", true);
 user_pref("firefoxpwa.displayUrlBar", 1);
+
+user_pref("gfx.webrender.all", true);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", true);
