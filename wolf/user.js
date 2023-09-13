da97a39ed0ae135ba8e624f8da6ee8cb935c9559
Bren <94324138+Acumane@users.noreply.github.com>
2023-09-13 18:32:42 -0400

    tmux, espando, misc.

--- wolf/user.js
+++ wolf/user.js
@@ -7,7 +7,6 @@ user_pref("devtools.debugger.prompt-connection", false);
 user_pref("devtools.debugger.remote-enabled", true);
 user_pref("full-screen-api.warning.timeout", 0);
 user_pref("mousewheel.default.delta_multiplier_y", 30);
-user_pref("gfx.webrender.all", true);
 
 user_pref("browser.uidensity", 0); // Normal UI density
 user_pref("gnomeTheme.hideSingleTab", true);
@@ -18,6 +17,6 @@ user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
 user_pref("firefoxpwa.enableHidingIconBar", true);
 user_pref("firefoxpwa.displayUrlBar", 1);
 
-user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.all", false);
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.hardware-video-decoding.force-enabled", true);
--- zsh/var.zsh
+++ zsh/var.zsh
@@ -7,12 +7,13 @@ export LOCAL="$HOME/.local/share/"
 
 export PROMPT_EOL_MARK=""
 
-export PATH=$PATH:/home/bren/.local/bin
+export PATH=$PATH:"$HOME/.local/bin"
+export PATH=$PATH:"$HOME/.cargo/bin"
 
-export PNPM_HOME="/home/bren/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 
-export PATH=$PATH:/home/bren/.spicetify
+export PATH=$PATH:"$HOME/.spicetify"
 
 export MODULAR_HOME="$HOME/.modular"
 export PATH="$MODULAR_HOME/pkg/packages.modular.com_mojo/bin:$PATH"
