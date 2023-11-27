58644787f8de7683d8056431914bff99790351bf
Bren <94324138+Acumane@users.noreply.github.com>
2023-11-26 21:19:41 -0500

    FF, homepage

--- wolf/user.js
+++ wolf/user.js
@@ -6,9 +6,10 @@ user_pref("browser.theme.dark-private-windows", false);
 user_pref("devtools.debugger.prompt-connection", false);
 user_pref("devtools.debugger.remote-enabled", true);
 user_pref("full-screen-api.warning.timeout", 0);
-user_pref("mousewheel.default.delta_multiplier_y", 30);
+// user_pref("mousewheel.default.delta_multiplier_y", 30); // laptop
+user_pref("mousewheel.default.delta_multiplier_y", 140);
 
-user_pref("browser.uidensity", 0); // Normal UI density
+user_pref("browser.uidensity", 0); // normal UI density
 user_pref("gnomeTheme.hideSingleTab", true);
 user_pref("gnomeTheme.systemIcons", true);
 user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
