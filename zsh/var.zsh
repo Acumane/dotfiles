cdfc22c19880a3cae33e8427475e1876f8290212
Bren <94324138+Acumane@users.noreply.github.com>
2023-12-02 16:57:00 -0500

    DDC control, tab+color fix, bt

--- zsh/var.zsh
+++ zsh/var.zsh
@@ -17,3 +17,7 @@ export PATH=$PATH:"$HOME/.spicetify"
 
 export MODULAR_HOME="$HOME/.modular"
 export PATH="$MODULAR_HOME/pkg/packages.modular.com_mojo/bin:$PATH"
+
+export FZF_DEFAULT_OPTS="--height 60% \
+--layout=reverse --border -m --bind 'space:toggle'"
+export HSA_OVERRIDE_GFX_VERSION=10.3.0
