1fffee5e2ce88c0573ac525a983fb27c8baecefb
Bren <94324138+Acumane@users.noreply.github.com>
2023-12-12 05:50:36 -0500

    Bespoke zle widgets, key fixes, zsh!

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,7 +3,7 @@
 alias reload="source $DOTS/zsh/zshrc"
 alias bundle="antigen bundle"
 alias using="antigen use"
-alias plug="zplug"
+alias load="zcomet load"
 
 alias ls="ls -CAF --color=auto"
 alias la="la -lAF --color=auto"
@@ -21,8 +21,13 @@ alias hibernate="systemctl hibernate"
 alias lock="xdg-screensaver lock"
 alias bios="systemctl reboot --firmware-setup"
 
+
 alias tar='tar -czvf'
 alias untar='\tar -xvf' 
+zip() {
+  command zip -r "$1.zip" "$1"/
+}
+#     unzip
 alias ping='ping -c 5'
 alias root="sudo -s"
 alias kernel="uname -r"
@@ -38,22 +43,78 @@ alias dla="yt-dlp -P ~/Downloads -x --audio-format mp3"
 alias pn="pnpm"
 alias py="python"
 alias app="sudo dnf5"
-alias open="nautilus"
+alias file="nautilus"
 alias fm="ranger"
-alias v="neovim"
+alias v="nvim"
 alias c="code"
 
-
 alias keys="showkey -a"
-alias f='grep --color -i'
-alias p='bat --color "always"'
-alias s='fzf --height 100% --border=none \
---preview "bat --color 'always' {}"'
-
-much() {
-	du -h -d 1 $1 | grep '[0-9]\+G'
-}
+alias f='rg -i'
+alias F='rg -i -n --context=1'
+alias re='perl -pe'
+alias p='bat --color=always'
 
 alias ..='cd ..'
 alias ...='cd ../../../'
 alias ....='cd ../../../../'
+
+alias wc='wc --words'
+alias lc='\wc --lines'
+much() {
+  du -h -d 1 $1 2>/dev/null | grep '[0-9]\+G'
+}
+
+# Validate commands* before appending to HISTFILE
+zshaddhistory() { whence ${${(z)1}[1]} > /dev/null || return 1 }
+
+# —— WIDGETS ————————————————————
+
+open() {
+  IFS=$'\n' files=($(fzf -m --query="$BUFFER" --preview 'bat -n --color=always {}' </dev/tty))
+  for file in "${files[@]}"; do
+    [[ -n "$file" ]] && xdg-open "$file" || return 1
+  done
+  zle reset-prompt
+}
+zle -N open
+
+search() {
+  if [ -z "$BUFFER" ]; then return 1; fi
+  files=($(rga -i --max-count=1 --color=always --line-number --no-heading "$BUFFER" |
+    perl -pe 's/([^:]+:[^:]+)(.*)/$1/' | perl -pe 's/\[35m//' |
+    fzf --ansi -m --delimiter : \
+        --preview 'bat -n --color=always {1} --highlight-line {2}' --preview-window 'top,40%,+{2}/2'))
+  for file in "${files[@]}"; do
+    [[ -n "$file" ]] && xdg-open "$file" || return 1
+  done
+  zle reset-prompt
+}
+zle -N search
+
+kill() {
+  pids=$(ps -u ${UID:-$(id -u)} -o pid,comm,cmd \
+  | fzf -m --query="$BUFFER" --header-lines=1 | awk '{print $1}')
+
+  if [ -n "$pids" ]; then echo $pids | xargs -r kill -${1:-9}; fi
+  zle reset-prompt
+}
+zle -N kill
+
+hist() {
+  BUFFER=$(history 1 | cut -f4- -d' ' | fzf +s --tac --query="$BUFFER")
+  zle reset-prompt; CURSOR=${#BUFFER}
+}
+zle -N hist
+
+where() {
+  RBUFFER=$(locate / | fzf +s)
+  zle reset-prompt; CURSOR=${#BUFFER}
+}
+zle -N where
+
+dir() {
+  dir=$(find -L . -mindepth 1 -path '*/.*' -prune -o -type d -print 2> /dev/null |
+  fzf --query="$BUFFER") && cd "$dir"
+  zle reset-prompt
+}
+zle -N dir
--- zsh/var.zsh
+++ zsh/var.zsh
@@ -4,6 +4,8 @@ export CACHE="$HOME/.cache"
 export CONFIG="$HOME/.config"
 export LOCAL="$HOME/.local/share/"
 
+export WORDCHARS=""
+export ZSHZ_OWNER="bren"
 export PROMPT_EOL_MARK=""
 export EDITOR="code"
 
@@ -18,6 +20,12 @@ export PATH=$PATH:"$HOME/.spicetify"
 export MODULAR_HOME="$HOME/.modular"
 export PATH="$MODULAR_HOME/pkg/packages.modular.com_mojo/bin:$PATH"
 
-export FZF_DEFAULT_OPTS="--height 60% \
---layout=reverse --border -m --bind 'space:toggle'"
+export FZF_DEFAULT_OPTS="
+--height=20 --info=inline-right --exit-0 --select-1 --color "hl+:-1:reverse"
+--preview-window '70%' --layout=reverse --pointer=█ --marker=▍
+--bind 'shift-delete:backward-kill-word'
+--bind 'C:execute-silent(wl-copy "{+}" --trim-newline)'
+--bind 'alt-p:change-preview-window(hidden|right)'
+--bind 'space:toggle'"
+
 export HSA_OVERRIDE_GFX_VERSION=10.3.0
