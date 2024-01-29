30a13b9e7445188b337e2475dd0b0765cacae2e3
Bren <94324138+Acumane@users.noreply.github.com>
2024-01-28 22:05:37 -0800

    More sounds

--- scripts/amute.sh
+++ scripts/amute.sh
@@ -8,6 +8,7 @@ if [ "$2" = "--default-source" ]; then
     brightnessctl -d hda::micmute set $( [ $MUTED = "true" ] && echo 1 || echo 0 )
     # [ "$1" = "-t" ] && notify-send "Source $( [ $MUTED = "true" ] && echo muted || echo unmuted )"
 else 
+    [ $1 != "-g" ] && play -qv 0.5 $HOME/audio/sounds/active.flac &
     sudo $LED 0x20 0x500 0x0b
     [ $MUTED = "true" ] && sudo $LED 0x20 0x400 0x08 || sudo $LED 0x20 0x400 0x00
     # [ "$1" = "-t" ] && notify-send "Sink $( [ $MUTED = "true" ] && echo muted || echo unmuted )"
