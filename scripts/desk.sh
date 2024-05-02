54be5d5c9a7f17e4532e3dea1915c7f5a4e183f0
Bren <94324138+Acumane@users.noreply.github.com>
2024-05-01 23:30:53 -0700

    Scroller changes, desk, visuals

--- scripts/desk.sh
+++ scripts/desk.sh
@@ -4,10 +4,14 @@ handle() {
   # "desktop" mode:
   case $1 in
     "monitoradded>>DP"*)
+      pkill -f idle-lap.sh && pkill -f rotate.sh
       hyprctl keyword monitor "eDP-1, disable"
       sudo modprobe -r intel_ish_ipc
+      # TODO: kbd *still* disables @ 360deg
       ;;
     "monitorremoved>>DP"*)
+      pgrep -f "idle-lap.sh" || sh $DOTS/scripts/idle.sh &
+      pgrep -f "rotate.sh" || sh $DOTS/scripts/rotate.sh &
       hyprctl keyword monitor "eDP-1, enable"
       sudo modprobe intel_ish_ipc
       ;;
