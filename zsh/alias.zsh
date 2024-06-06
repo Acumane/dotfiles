d66a1fbbe188e8c0622342f75a63dae567044404
Bren <git@bren.page>
2024-06-05 19:28:33 -0700

    zsh aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -14,13 +14,12 @@ alias cp="cp -r"
 alias del="rm -r"
 alias rm="trash-put"
 alias trash="trash-list"
-alias urm="trash-restore"
 alias restore="trash-restore"
 alias dump="trash-empty -f"
 alias rn="mv"
 mk() {
-  if [[ "${1: -1}" == "/" ]]; then mkdir -p "${1:0:-1}"
-  else touch "$1"; fi
+  [[ "${1: -1}" == "/" ]] && mkdir -p "${1:0:-1}" \
+  || touch "$1"
 }
 alias mkd="mkdir -p"
 alias chg="entr -pc"
@@ -40,7 +39,8 @@ alias bios="sudo systemctl reboot --firmware-setup"
 alias tar='tar -czvf'
 untar() { \tar -xvf "$1" --one-top-level="$2"; }
 zip() { command zip -r "$1.zip" "$1"/; }
-#     unzip
+# unzip
+alias calc="bc"
 alias ping='ping -c 5'
 alias root="sudo -s"
 alias kernel=" uname -r"
@@ -58,6 +58,8 @@ ip() {
 
 alias speed='fast -u --single-line'
 alias clock="darshellclock"
+vault() { setopt LOCAL_OPTIONS NO_MONITOR
+flatpak run io.github.mpobaschnig.Vaults -o .enc/"$1" &> /dev/null; }
 alias batt="acpi -b | grep -v 'rate' | cut -d' ' -f 3-"
 alias wifi="nmcli dev wifi"
 alias udev="udevadm"
@@ -77,19 +79,21 @@ alias py="python"
 alias app="sudo dnf5"
 alias copr="sudo dnf copr"
 ver() { dnf list "$1" | grep "$1" | tr -s ' ' | cut -d' ' -f 2; }
-alias files="nautilus"
-alias handle="handlr"
 alias open="handlr open"
-alias fm="ranger"
-alias mon="zfxtop"
-alias v="nvim"
+alias handle="handlr"
 alias c="code"
+alias fm="ranger"
+v() { nvim 2> /dev/null; }
+t() { nvim -c ':terminal' 2> /dev/null; }
+zle -N v; zle -N t; zle -N fm
+
 
 alias keys="showkey -a"
 alias f='rg -iP'
 alias F="grep --color=always --group-separator=$'\n———\n' -C3 -iE"
+alias fp="pgrep"
 # F() {grep --color=always --group-separator=$'\n———\n' -C3 -iE -- "$1" "${@:2}" | less -R; }
-# ighlight() { grep --color -E -- "$1|\$" "${@:2}"; }
+# hl() { grep --color -E -- "$1|\$" "${@:2}"; }
 alias re='perl -pe'
 alias p='bat --style=numbers,changes,grid --color=always --tabs=2'
 alias pi="kitten icat"
@@ -140,22 +144,23 @@ opener() {
     --bind='alt-v:reload(fd -0LHI --type=f --exclude=.git --max-depth=4 --color=always)' \
     --preview 'bat -n --color=always {}'))
   for file in "${files[@]}"; do
-    xdg-open "$file" & disown &> /dev/null
+    xdg-open "$file" &> /dev/null
   done
   zle reset-prompt
 }
 zle -N opener
 
 search() {
-  if [ -z "$BUFFER" ]; then return 1; fi
+  [ -z "$BUFFER" ] && return 1
   IFS=$'\n'; setopt LOCAL_OPTIONS NO_MONITOR
   files=($(rga -i --no-messages --max-count=1 --line-number --field-match-separator '\x00' --no-heading "$BUFFER" |
     perl -pe 's/(.+)\x00(\d+)\x00(.*)/`echo "$1" | lscolors | tr -d "\n"` . ":" . $2/e' |
     fzf -m --delimiter : --preview 'bat -n --color=always {1} --highlight-line {2}' --preview-window '+{2}/2'))
   for file in "${files[@]}"; do
     name=$(echo "$file" | perl -pe 's/(.+):\d+$/$1/')
-    xdg-open "$name" & disown &> /dev/null
+    xdg-open "$name" &> /dev/null
   done
+  BUFFER=""
   zle reset-prompt
 }
 zle -N search
@@ -189,5 +194,9 @@ dirs() {
 }
 zle -N dirs
 
-explore() { files . &> /dev/null & disown; zle reset-prompt; }
-zle -N explore
+files() { 
+  setopt LOCAL_OPTIONS NO_MONITOR
+  local DIR="${1:-.}"
+  nautilus "$DIR" &> /dev/null & disown
+}
+zle -N files
