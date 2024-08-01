31b378b707b218da8c65cef4ade66af5456ff683
Bren <git@bren.page>
2024-08-01 16:34:05 -0700

    SwayOSD

--- scripts/amute.sh
+++ scripts/amute.sh
@@ -5,11 +5,12 @@ LED="hda-verb /dev/snd/hwC0D0"
 
 # Source/sink: set brightness, notify
 if [ "$2" = "--default-source" ]; then
+    swayosd-client --input-volume 0
     brightnessctl -d hda::micmute set $( [ $MUTED = "true" ] && echo 1 || echo 0 )
     # [ "$1" = "-t" ] && notify-send "Source $( [ $MUTED = "true" ] && echo muted || echo unmuted )"
 else 
     [ $1 != "-g" ] && play -qv 0.5 $HOME/audio/sounds/active.flac &
-    sudo $LED 0x20 0x500 0x0b
+    sudo $LED 0x20 0x500 0x0b && swayosd-client --output-volume 0
     [ $MUTED = "true" ] && sudo $LED 0x20 0x400 0x08 || sudo $LED 0x20 0x400 0x00
     # [ "$1" = "-t" ] && notify-send "Sink $( [ $MUTED = "true" ] && echo muted || echo unmuted )"
 fi
--- scripts/light.sh
+++ scripts/light.sh
@@ -6,8 +6,7 @@ CUR=$(brillo -G)
 STEP=$((.05*$SIZE*$CUR+2))
 
 case $1 in
-    "+"*)
-        brillo -A ${STEP%.*} -u 100000 ;;
-    "-"*)
-        brillo -U ${STEP%.*} -u 100000
+    +*) brillo -A ${STEP%.*} -u 100000 ;;
+    -*) brillo -U ${STEP%.*} -u 100000
 esac
+swayosd-client --brightness +0
