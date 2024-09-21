5fa4d2cf4f32631844b6189d6a6083c5a45ce229
Bren <git@bren.page>
2024-09-21 15:32:23 -0400

    idle updates, ext_dpms_off()

--- scripts/idle-lap.sh
+++ scripts/idle-lap.sh
@@ -1,27 +1,34 @@
 # Enter DPMS standby after 60s idle on lockscreen
+
 DOTS="/home/bren/dotfiles"
 exec &> /dev/null
 
-# User-activated (-f): force standby in 1.5s
+ext_dpms_off() {
+    mons=$(hyprctl monitors -j | jq -r '.[1:][].name')
+    if [ -n "$mons" ]; then
+        while IFS= read -r mon; do hyprctl dispatch dpms off "$mon"
+        done <<< "$mons"
+    fi
+}
+
+# User-activated (-f): force standby
 if [ "$2" = "-f" ]; then
-    # no x01; ddcutil's --usb can't find, hub must forget device
-    # ddccontrol -r 0xd6 -w 5 $1 &
     swaylock --conf="$DOTS/sway/lock.conf" --grace=0 &
-    sleep .5s; brillo -O; brillo -S 0% -u 1000000; blight set 0 & # wlsunset hates dpms
+    sleep .5s; brillo -O; brillo -S 0% -u 1000000; blight set 0 & ext_dpms_off
     swayidle -w timeout 1 '' resume "brillo -I -u 100000 && kill \$(pgrep -n swayidle)"
     exit
 fi
 
 # Immediate (-i): lockscreen only
-[ "$2" = "-i" ] && { swaylock --conf="$DOTS/sway/lock.conf" --grace=0 & exit; }
+[ "$1" = "-i" ] && { swaylock --conf="$DOTS/sway/lock.conf" --grace=0 & exit; }
 
 # Passive (battery): fade (50s), lock (1m30s), backlight off (1m33s), suspend (2m30s)
 swayidle \
 timeout 50 "(acpi -a | grep -q off) && (brillo -O; brillo -S 0% -u 3000000)" resume "pkill brillo; brillo -I -u 150000" \
-timeout 90 "(acpi -a | grep -q off) && (swaylock --conf='$DOTS/sway/lock.conf' --grace-no-mouse & ddccontrol -r 0xd6 -w 5 $1)" \
+timeout 90 "(acpi -a | grep -q off) && (swaylock --conf='$DOTS/sway/lock.conf' --grace-no-mouse & ext_dpms_off)" \
 timeout 93 "(acpi -a | grep -q off) && blight set 0" resume "brillo -I -u 150000" \
 timeout 150 "(acpi -a | grep -q off) && systemctl suspend" after-resume "brillo -I -u 150000" \
 timeout 170 "(acpi -a | grep -q on) && (brillo -O; brillo -S 0% -u 3000000)" resume "pkill brillo; brillo -I -u 150000" \
-timeout 210 "(acpi -a | grep -q on) && (swaylock --conf='$DOTS/sway/lock.conf' --grace-no-mouse & ddccontrol -r 0xd6 -w 5 $1)" \
+timeout 210 "(acpi -a | grep -q on) && (swaylock --conf='$DOTS/sway/lock.conf' --grace-no-mouse & ext_dpms_off)" \
 timeout 213 "(acpi -a | grep -q on) && blight set 0" resume "brillo -I -u 150000" \
 # Passive (charging): fade (2m50s), lock (3m30s), backlight off (3m33s)
--- scripts/idle.sh
+++ scripts/idle.sh
@@ -2,14 +2,14 @@
 DOTS="/home/bren/dotfiles"
 exec &> /dev/null
 
-# User-activated (-f): force standby in 2s
-if [ "$2" = "-f" ]; then
+# User-activated (-f): force standby
+if [ "$1" = "-f" ]; then
     swaylock --conf="$DOTS/sway/lock.conf" --grace=0 &
-    sleep 2 && ddccontrol -r 0xd6 -w 4 $1
-    swayidle -w timeout 1 '' resume "ddccontrol -r 0xd6 -w 1 $1 && kill \$(pgrep -n swayidle)"
+    sleep 2 && hyprctl dispatch dpms off
+    swayidle -w timeout 1 '' resume "kill \$(pgrep -n swayidle)"
 fi
 
-wayidle -t 60 ddccontrol -r 0xd6 -w 4 $1 & iPID=$!
+wayidle -t 60 hyprctl dispatch dpms off & iPID=$!
 swaylock --conf="$DOTS/sway/lock.conf" --grace-no-mouse
 if [[ $? -eq 0 || $? -eq 2 ]]; then
     kill $iPID
