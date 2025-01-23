020827e4b78bc8c7f50109e9854efe82a01de0c1
Bren <git@bren.page>
2025-01-23 15:05:49 -0800

    Rofi updates

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -9,18 +9,16 @@ c=$(hyprctl monitors -j  | jq ".[] | select(.focused == true) | .width / 2 + $x0
 w=""
 case $1 in
     "run")  rofi -show drun -normal-window -show-icons & w="325";;
-    "copy") w="375" && cliphist list | rofi -normal-window -dmenu -display-columns 2 -p "COPY" -theme-str "listview{lines:6;columns:1;} window{width:${w}px;}" \
+    "copy") w="375" && cliphist list | rofi -normal-window -dmenu -display-columns 2 -p "" -theme-str "listview{lines:6;columns:1;} window{width:${w}px;}" \
             | cliphist decode | wl-copy & w="375";;
     "emoji") rofimoji --selector-args="-normal-window \
             -theme-str 'listview{lines:4;columns:6;fixed-columns:true;flow:horizontal;} element-text{font:\"Akkurat 13\";} element{padding:11px;margin:3px;}'" \
-            --hidden-descriptions -s neutral --max-recent 0 -r EMOJI & w="325";;
-    "cmd")  zsh -c "$(rofi -normal-window -dmenu -p "CMD" -theme-str 'listview{enabled:false;} entry{font:"JetBrains Mono 10";} window{width:450px;}' &)" & w="450"
+            --hidden-descriptions -s neutral --max-recent 0 -r ''  & w="325";;
 esac
 
 while ! hyprctl clients | grep -q "class: Rofi"; do sleep 0.05; done
 
 pkill -USR1 waybar
-hyprctl dispatch "movewindowpixel exact $((c - (w/2))) $y, class:Rofi"
 
 { while pgrep -x rofi > /dev/null; do # kill if focus is lost
     if [[ $(hyprctl activewindow -j | jq -r .class) != "Rofi" ]]; then
