6caf77041427333e8a041e6d31ea2656230cb85d
Bren <git@bren.page>
2025-01-28 14:55:07 -0800

    Minor updates 1/28

--- firefox/user.js
+++ firefox/user.js
@@ -1,5 +1,6 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+user_pref("browser.urlbar.ctrlCanonizesURLs", false);
 user_pref("ui.key.menuAccessKeyFocuses", false);
 // user_pref("permissions.default.shortcuts", 2);
 user_pref("findbar.highlightAll", true);
--- scripts/modes.sh
+++ scripts/modes.sh
@@ -7,14 +7,12 @@ GAME=$(hyprctl getoption animations:enabled -j | jq ".int")
 
 if [[ "$GAME" = 1 ]]; then
     ratbagctl "G900" led 0 set color ff0f00; ratbagctl "G900" rate set 1000
-    liquidctl --match h80i set logo color blackout --alert-threshold 85
     openrgb -p game.orp
     hyprctl keyword animations:enabled 0
     notify-send -u low -i - "Switched to game mode"
 
 elif [[ "$GAME" = 0 ]]; then
     ratbagctl "G900" led 0 set color 6e6482; ratbagctl "G900" rate set 250
-    liquidctl --match h80i set logo color fixed 75a6ff --alert-threshold 60
     openrgb -p main.orp
     hyprctl keyword animations:enabled 1
     notify-send -u low -i - "Switched to main mode"
