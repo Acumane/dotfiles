ff2efe883f9229051eb9e38001e1fb8271ef425a
Bren <git@bren.page>
2025-02-01 23:57:11 -0800

    feat(weather): climate status as needed

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -4,9 +4,22 @@ cache="/tmp/weather"
 now=$(date +%s)
 
 if [ -f "$cache" ] && [ $((now - $(stat -c %Y "$cache"))) -lt 1800 ]; then
-    cat "$cache" # Use cachce <30m old
+    cat "$cache" # Use cache <30m old
 else
-    weather=$(curl -s 'wttr.in/?format=%c+%f' 2>&1)
-    [[ $weather =~ ^(Unknown).+ ]] && echo "⚠️" | tee "$cache" && exit 1
-    echo "$weather" | sed 's/+//g;s/F//g' | tee "$cache"
-fi | awk "{print $1}"
+    weather=$(curl -s 'wttr.in/?format=%f+%w+%p+%u' 2>&1)
+    if [[ -z "$weather" || $weather =~ ^(Unknown).+ ]]; then
+        echo "⚠️" && exit 1
+    fi
+    echo "$weather" \
+    | sed 's/+//g;s/F//g;s/[→←↑↓]//g;s/mph//;s/mm//' \
+    | tee "$cache"
+# UV index, rain, wind warnings:
+fi | awk '{
+    temp=$1; wind=$2; precip=$3; uv=$4
+
+    if (uv > 5) icon="☀️"
+    else if (precip > 1.0) icon="💦"; else if (precip > 0) icon="💧"
+    else if (wind > 20) icon="💨"
+
+    print temp, icon
+}' | awk "{print $1}"
