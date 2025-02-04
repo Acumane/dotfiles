4221709ad1f1f040a7a7a5a3d11404658e11be02
Bren <git@bren.page>
2025-02-03 18:27:05 -0800

    feat(weather): trends, snow, humidity

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -6,20 +6,35 @@ now=$(date +%s)
 if [ -f "$cache" ] && [ $((now - $(stat -c %Y "$cache"))) -lt 1800 ]; then
     cat "$cache" # Use cache <30m old
 else
-    weather=$(curl -s 'wttr.in/?format=%f+%w+%p+%u' 2>&1)
-    if [[ -z "$weather" || $weather =~ ^(Unknown).+ ]]; then
-        echo "⚠️" && exit 1
-    fi
-    echo "$weather" \
-    | sed 's/+//g;s/F//g;s/[→←↑↓]//g;s/mph//;s/mm//' \
-    | tee "$cache"
-# UV index, rain, wind warnings:
-fi | awk '{
-    temp=$1; wind=$2; precip=$3; uv=$4
+    weather=$(curl -s 'wttr.in/?format=%f+%w+%p+%u' 2>&1 | sed 's/[^0-9. ]//g')
+    [[ -z "$weather" || $weather =~ ^(Unknown).+ ]] && echo "⚠️" && exit 1
 
-    if (uv > 5) icon="☀️"
-    else if (precip > 1.0) icon="💦"; else if (precip > 0) icon="💧"
-    else if (wind > 20) icon="💨"
+    data=$(curl -s 'wttr.in/?format=j1' 2>&1)
+    cur=$(( $(date +%H) / 3 )) # current time block ([0-7] x 3h)
+    now=".weather[0].hourly[$cur]"
+    t=$(echo "$data" | jq -r ".weather[0].hourly[$cur].tempF")
 
-    print temp, icon
-}' | awk "{print $1}"
+    if [ $cur -eq 7 ]; then t_soon=$(echo "$data" | jq -r '.weather[1].hourly[0].tempF')
+    else t_soon=$(echo "$data" | jq -r ".weather[0].hourly[$((cur + 1))].tempF"); fi
+
+    trend=""
+    if (( $(echo "$t_soon - $t >= 1" | bc -l) )); then trend="🔺"
+    elif (( $(echo "$t - $t_soon >= 1" | bc -l) )); then trend="🔻"; fi
+
+    read -r snow rain humid <<< \
+        "$(echo "$data" | jq -r "$now | [.chanceofsnow, .chanceofrain, .humidity] | @tsv")"
+
+    echo "$weather $rain $snow $humid $trend" | tee "$cache"
+    # echo "90 30 3.0 8 51 51 84 $trend" | tee "$cache"
+fi | while read temp wind precip uv rain snow humid trend; do
+    icon=""
+    
+    (( uv > 5 )) && icon="☀️"
+    (( temp > 80 && humid > 70 )) && icon+="♨️"
+    (( rain > 50 )) && icon="💧"
+    (( rain > 50 )) &&  [ "$(echo "$precip > 1" | bc -l)" -eq 1 ] && icon="🌧️"
+    (( snow > 50 )) && icon="❄️"
+    (( wind > 20 )) && icon+="💨"
+
+    echo "$temp° $trend⠀$icon"
+done | awk "{print $1}"
