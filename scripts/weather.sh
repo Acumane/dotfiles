abb11764c63174d019216405654160847412e189
Bren <git@bren.page>
2025-02-06 22:08:52 -0800

    feat(weather): use current_condition

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -6,35 +6,33 @@ now=$(date +%s)
 if [ -f "$cache" ] && [ $((now - $(stat -c %Y "$cache"))) -lt 1800 ]; then
     cat "$cache" # Use cache <30m old
 else
-    weather=$(curl -s 'wttr.in/?format=%f+%w+%p+%u' 2>&1 | sed 's/[^0-9. ]//g')
-    [[ -z "$weather" || $weather =~ ^(Unknown).+ ]] && echo "⚠️" && exit 1
-
     data=$(curl -s 'wttr.in/?format=j1' 2>&1)
-    cur=$(( $(date +%H) / 3 )) # current time block ([0-7] x 3h)
-    now=".weather[0].hourly[$cur]"
-    t=$(echo "$data" | jq -r ".weather[0].hourly[$cur].tempF")
+    [[ -z "$data" || $data =~ ^(Unknown).+ ]] && echo "⚠️" && exit 1
+ 
+    i=$(( $(date +%H) / 3 )) # current time block ([0-7] x 3h)
+    block=".weather[0].hourly[$i]"
+    cur=".current_condition[0]"
+    t=$(echo "$data" | jq -r "$block.tempF")
 
-    if [ $cur -eq 7 ]; then t_soon=$(echo "$data" | jq -r '.weather[1].hourly[0].tempF')
-    else t_soon=$(echo "$data" | jq -r ".weather[0].hourly[$((cur + 1))].tempF"); fi
+    if [ $i -eq 7 ]; then t_soon=$(echo "$data" | jq -r '.weather[1].hourly[0].tempF')
+    else t_soon=$(echo "$data" | jq -r ".weather[0].hourly[$((i + 1))].tempF"); fi
 
     trend=""
     if (( $(echo "$t_soon - $t >= 1" | bc -l) )); then trend="🔺"
     elif (( $(echo "$t - $t_soon >= 1" | bc -l) )); then trend="🔻"; fi
 
-    read -r snow rain humid <<< \
-        "$(echo "$data" | jq -r "$now | [.chanceofsnow, .chanceofrain, .humidity] | @tsv")"
-
-    echo "$weather $rain $snow $humid $trend" | tee "$cache"
-    # echo "90 30 3.0 8 51 51 84 $trend" | tee "$cache"
-fi | while read temp wind precip uv rain snow humid trend; do
+    read -r desc <<< "$(echo "$data" | jq -r "$cur.weatherDesc[0].value")"
+    read -r temp wind uv <<< \
+        "$(echo "$data" | jq -r "$cur | [.FeelsLikeF, .windspeedMiles, .uvIndex] | @tsv")"
+    dewpt=$(echo "$data" | jq -r "$block.DewPointF") # wttr.in #738
     icon=""
-    
-    (( uv > 5 )) && icon="☀️"
-    (( temp > 80 && humid > 70 )) && icon+="♨️"
-    (( rain > 50 )) && icon="💧"
-    (( rain > 50 )) &&  [ "$(echo "$precip > 1" | bc -l)" -eq 1 ] && icon="🌧️"
-    (( snow > 50 )) && icon="❄️"
-    (( wind > 20 )) && icon+="💨"
 
-    echo "$temp° $trend⠀$icon"
-done | awk "{print $1}"
+    (( uv > 5 )) && icon="☀️"
+    [[ $desc =~ (rain|drizzle) ]] && icon="💧"
+    [[ $desc =~ ([Mm]oderate|[Hh]eavy)\ .*(rain) ]] && icon="🌧️"
+    [[ $desc =~ (snow|sleet) ]] && icon="❄️"
+    (( dewpt >= 65 )) && icon+="♨️"
+    (( wind >= 20 )) && icon+="💨"
+
+    echo "$temp° $trend⠀$icon" | tee "$cache"
+fi 2> /dev/null | awk "{print $1}"
