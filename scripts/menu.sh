811cc4a47267442927a61382a33744b46fbcc5d9
Bren <git@bren.page>
2025-02-16 15:19:01 -0800

    fix(Rofi): cleanup, emoji copy

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -8,12 +8,12 @@ c=$(hyprctl monitors -j  | jq ".[] | select(.focused == true) | .width / 2 + $x0
 
 w=""
 case $1 in
-    "run")  rofi -show drun -normal-window -show-icons & w="325";;
+    "run")  rofi -show drun -normal-window -show-icons;;
     "copy") w="375" && cliphist list | rofi -normal-window -dmenu -display-columns 2 -p "" -theme-str "listview{lines:6;columns:1;} window{width:${w}px;}" \
-            | cliphist decode | wl-copy & w="375";;
-    "emoji") rofimoji --selector-args="-normal-window \
+            | cliphist decode | wl-copy;;
+    "emoji") rofimoji --action clipboard --selector-args="-normal-window \
             -theme-str 'listview{lines:4;columns:6;fixed-columns:true;flow:horizontal;} element-text{font:\"Akkurat 13\";} element{padding:11px;margin:3px;}'" \
-            --hidden-descriptions -s neutral --max-recent 0 -r ''  & w="325";;
+            --hidden-descriptions -s neutral --max-recent 0 -r '';;
 esac
 
 while ! hyprctl clients | grep -q "class: Rofi"; do sleep 0.05; done
