0ad45fc225082579d75c6724b4a0b97f32854bba
Bren <git@bren.page>
2025-03-04 14:27:59 -0800

    Cleanup old scripts

--- scripts/lec.sh
+++
@@ -1,5 +0,0 @@
-if [ $1 = "fetch" ]; then
-	edge-tts --text "$(xsel)" --rate=+80% 2>/dev/null | mpv --input-ipc-server=/tmp/mpvsocket -
-elif [ $1 = "pause" ]; then
-	echo '{ "command": ["cycle", "pause"] }' | socat - /tmp/mpvsocket
-fi
