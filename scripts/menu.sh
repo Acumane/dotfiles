1e4053dee831997d24935e0c3f0cc070e47e64f3
Bren <git@bren.page>
2025-05-09 20:02:30 -0700

    fontconfig: set/use generic

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -5,6 +5,6 @@ pkill -x rofi
 case $1 in
     "run")   rofi -show drun -show-icons;;
     "copy")  cliphist list | rofi -dmenu -display-columns 2 -p "" -theme-str "listview{lines:6;columns:1;} window{width:375px;}" | cliphist decode | wl-copy;;
-    "emoji") rofimoji --action clipboard --selector-args="-theme-str 'listview{lines:4;columns:6;fixed-columns:true;flow:horizontal;} element-text{font:\"Akkurat 13\";} element{padding:11px;margin:3px;}'" \
+    "emoji") rofimoji --action clipboard --selector-args="-theme-str 'listview{lines:4;columns:6;fixed-columns:true;flow:horizontal;} element-text{font:\"system-ui 13\";} element{padding:11px;margin:3px;}'" \
             --hidden-descriptions -s neutral --max-recent 0 -r '';;
 esac
