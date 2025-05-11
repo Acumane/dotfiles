83385673cf6d8678d6276df1547b874f0e2f2e39
Bren <git@bren.page>
2025-05-10 18:12:11 -0700

    Font scale issues; firefox: tab-groups

--- firefox/user.js
+++ firefox/user.js
@@ -1,35 +1,38 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("media.videocontrols.picture-in-picture.enable-when-switching-tabs.enabled", true);
 user_pref("browser.urlbar.update2.engineAliasRefresh", true);
 user_pref("browser.urlbar.ctrlCanonizesURLs", false);
 user_pref("browser.tabs.insertAfterCurrent", true);
-user_pref("ui.key.menuAccessKeyFocuses", false);
 user_pref("browser.tabs.allowTabDetach", false);
-// user_pref("permissions.default.shortcuts", 2);
-user_pref("findbar.highlightAll", true);
+user_pref("browser.uidensity", 0); // normal UI density
+user_pref("browser.chrome.toolbar_tips", false);
+user_pref("browser.urlbar.trimHttps", true);
 
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.theme.dark-private-windows", false);
 user_pref("devtools.debugger.prompt-connection", false);
 user_pref("devtools.debugger.remote-enabled", true);
 user_pref("full-screen-api.warning.timeout", 0);
-user_pref("mousewheel.default.delta_multiplier_y", 30); // laptop
-user_pref("mousewheel.min_line_scroll_amount", 75);     // laptop
+user_pref("mousewheel.default.delta_multiplier_y", 30);
 // user_pref("mousewheel.default.delta_multiplier_y", 140);
+user_pref("mousewheel.min_line_scroll_amount", 75);
 
-user_pref("browser.uidensity", 0); // normal UI density
-user_pref("gnomeTheme.hideSingleTab", true);
-user_pref("gnomeTheme.systemIcons", true);
-user_pref("gnomeTheme.bookmarksToolbarUnderTabs", true);
-// user_pref("gnomeTheme.hideUnifiedExtensions", true);
+user_pref("gnomeTheme.activeTabContrast", true);
+user_pref("gnomeTheme.normalWidthTabs", true);
 
 user_pref("firefoxpwa.enableHidingIconBar", true);
 user_pref("firefoxpwa.displayUrlBar", 1);
 
 user_pref("gfx.webrender.all", true);
+// user_pref("dom.ipc.processCount", 12);
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.hardware-video-decoding.force-enabled", true);
 user_pref("extensions.screenshots.disabled", true);
 
+// user_pref("permissions.default.shortcuts", 2);
+user_pref("findbar.highlightAll", true);
+
+user_pref("ui.key.menuAccessKeyFocuses", false);
 // user_pref("ui.textHighlightBackground", "#EBBEFF");
 user_pref("ui.textHighlightBackground", "#F8CCAF");
 user_pref("ui.textHighlightForeground", "#000");
