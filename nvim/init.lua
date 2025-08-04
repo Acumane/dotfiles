5cd6af35c9e0f6f46d941dcf0c8bf6f9e4c31672
Bren Paul <brenpaul@machindustries.com>
2025-08-03 21:18:36 -0700

    Consolidate .zsh, setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,8 @@
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazy_path) then
+  local lazy_repo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazy_repo, lazy_path })
+end
 vim.opt.rtp:prepend(lazy_path)
 
 require("lazy").setup({
