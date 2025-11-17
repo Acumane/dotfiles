8989e7a8d968cb3d84684a2996f4c81a9271bc69
Bren <git@bren.page>
2025-11-16 19:52:37 -0800

    fixes

--- scripts/amute.sh
+++ scripts/amute.sh
@@ -10,7 +10,7 @@ if [ -n "$mic" ]; then
     brightnessctl -d hda::micmute set $( [ $MUTED = "true" ] && echo 1 || echo 0 )
     # [ "$1" = "-t" ] && notify-send "Source $( [ $MUTED = "true" ] && echo muted || echo unmuted )"
 else 
-    [ $1 != "-g" ] && swayosd-client --output-volume 0 && play -qv 0.5 $HOME/audio/sounds/active.flac &
+    [ $1 != "-g" ] && swayosd-client --output-volume 0 && play -qv 0.5 $HOME/audio/sounds/unmute.flac &
     sudo $LED 0x20 0x500 0x0b
     [ $MUTED = "true" ] && sudo $LED 0x20 0x400 0x08 || sudo $LED 0x20 0x400 0x00
     # [ "$1" = "-t" ] && notify-send "Sink $( [ $MUTED = "true" ] && echo muted || echo unmuted )"
